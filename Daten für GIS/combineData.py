a0f42dce9559fb590a4e035bc58641cb4eaa31d3
Ummel Dominik <dominik.ummel@students.unibe.ch>
2021-05-25 15:21:37 +0200

    rework data for gis

--- Daten für GIS/combineData.py
+++ Daten für GIS/combineData.py
@@ -53,8 +53,8 @@ def findCwsMeta(cwsno):
 
 #22
 index = 1
-while allnight[index][3][:-2] == "22":
-    concattedData22.append(["Bicycle", "26.06.2019 " + allnight[index][3][:2] + ":" + allnight[index][3][-2:], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
+while allnight[index][16][-8:-6] == "22":
+    concattedData22.append(["Bicycle", allnight[index][16], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
     index += 1
 
 for i in range(2,len(log[0])):
@@ -68,7 +68,7 @@ index = 2
 while index < len(cws[0]):
     cwsno = cws[0][index][:4]
     cwsM = findCwsMeta(cwsno)
-    concattedData22.append(["CWS", "26.06.2019 22:00", cws[3][index], cwsM[0], cwsM[1], cwsM[2]])
+    concattedData22.append(["CWS", cws[3][index-1], cws[3][index], cwsM[0], cwsM[1], cwsM[2]])
     index += 2
 
 with open("Data/concattedData22.csv", 'w', newline="") as output:
@@ -76,9 +76,9 @@ with open("Data/concattedData22.csv", 'w', newline="") as output:
     writer.writerows(concattedData22)
 
 #23
-index = 353
-while allnight[index][3][:-2] == "23":
-    concattedData23.append(["Bicycle", "26.06.2019 " + allnight[index][3][:2] + ":" + allnight[index][3][-2:], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
+index = 232
+while allnight[index][16][-8:-6] == "23":
+    concattedData23.append(["Bicycle", allnight[index][16], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
     index += 1
 
 for i in range(2,len(log[0])):
@@ -92,65 +92,65 @@ index = 2
 while index < len(cws[0]):
     cwsno = cws[0][index][:4]
     cwsM = findCwsMeta(cwsno)
-    concattedData23.append(["CWS", "26.06.2019 23:00", cws[4][index], cwsM[0], cwsM[1], cwsM[2]])
+    concattedData23.append(["CWS", cws[4][index-1], cws[4][index], cwsM[0], cwsM[1], cwsM[2]])
     index += 2
 
 with open("Data/concattedData23.csv", 'w', newline="") as output:
     writer = csv.writer(output, delimiter=",")
     writer.writerows(concattedData23)
 
-#02
-index = 509
-while allnight[index][3][:-2] == "2":
-    concattedData02.append(["Bicycle", "26.06.2019 " + allnight[index][3][:-2] + ":" + allnight[index][3][-2:], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
+#00
+index = 591
+while allnight[index][16][-8:-6] == "00":
+    concattedData02.append(["Bicycle", allnight[index][16], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
     index += 1
 
 for i in range(2,len(log[0])):
     logno = log[0][i][4:]
     logM = findLogMeta(logno)
 
-    for j in range(37,43):
+    for j in range(35,31):
         concattedData02.append(["Logger", log[j][0], log[j][i], logM[0], logM[1], logM[2]])
 
 index = 2
 while index < len(cws[0]):
     cwsno = cws[0][index][:4]
     cwsM = findCwsMeta(cwsno)
-    concattedData02.append(["CWS", "27.06.2019 02:00", cws[7][index], cwsM[0], cwsM[1], cwsM[2]])
+    concattedData02.append(["CWS", cws[5][index-1], cws[5][index], cwsM[0], cwsM[1], cwsM[2]])
     index += 2
 
-with open("Data/concattedData02.csv", 'w', newline="") as output:
+with open("Data/concattedData00.csv", 'w', newline="") as output:
     writer = csv.writer(output, delimiter=",")
     writer.writerows(concattedData02)
 
-#03
-index = 698
-while allnight[index][3][:-2] == "3":
-    concattedData03.append(["Bicycle", "26.06.2019 " + allnight[index][3][:-2] + ":" + allnight[index][3][-2:], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
+#01
+index = 943
+while allnight[index][16][-8:-6] == "01":
+    concattedData03.append(["Bicycle", allnight[index][16], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
     index += 1
 
 for i in range(2,len(log[0])):
     logno = log[0][i][4:]
     logM = findLogMeta(logno)
 
-    for j in range(43,49):
+    for j in range(31,37):
         concattedData03.append(["Logger", log[j][0], log[j][i], logM[0], logM[1], logM[2]])
 
 index = 2
 while index < len(cws[0]):
     cwsno = cws[0][index][:4]
     cwsM = findCwsMeta(cwsno)
-    concattedData03.append(["CWS", "27.06.2019 03:00", cws[8][index], cwsM[0], cwsM[1], cwsM[2]])
+    concattedData03.append(["CWS", cws[6][index-1], cws[6][index], cwsM[0], cwsM[1], cwsM[2]])
     index += 2
 
-with open("Data/concattedData03.csv", 'w', newline="") as output:
+with open("Data/concattedData01.csv", 'w', newline="") as output:
     writer = csv.writer(output, delimiter=",")
     writer.writerows(concattedData03)
 
 #04
-index = 1041
-while allnight[index][3][:-2] == "4":
-    concattedData04.append(["Bicycle", "26.06.2019 " + allnight[index][3][:-2] + ":" + allnight[index][3][-2:], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
+index = 1099
+while allnight[index][16][-8:-6] == "04":
+    concattedData04.append(["Bicycle", allnight[index][16], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
     index += 1
 
 for i in range(2,len(log[0])):
@@ -164,7 +164,7 @@ index = 2
 while index < len(cws[0]):
     cwsno = cws[0][index][:4]
     cwsM = findCwsMeta(cwsno)
-    concattedData04.append(["CWS", "27.06.2019 04:00", cws[9][index], cwsM[0], cwsM[1], cwsM[2]])
+    concattedData04.append(["CWS", cws[9][index-1], cws[9][index], cwsM[0], cwsM[1], cwsM[2]])
     index += 2
 
 with open("Data/concattedData04.csv", 'w', newline="") as output:
@@ -172,9 +172,11 @@ with open("Data/concattedData04.csv", 'w', newline="") as output:
     writer.writerows(concattedData04)
 
 #05
-index = 1327
-while allnight[index][3][:-2] == "5":
-    concattedData05.append(["Bicycle", "26.06.2019 " + allnight[index][3][:-2] + ":" + allnight[index][3][-2:], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
+index = 1288
+while allnight[index][16][-8:-6] == "05":
+    concattedData05.append(["Bicycle", allnight[index][16], allnight[index][5], allnight[index][13], allnight[index][14], allnight[index][15]])
+    if index == len(allnight)-1:
+        break
     index += 1
 
 for i in range(2,len(log[0])):
@@ -188,7 +190,7 @@ index = 2
 while index < len(cws[0]):
     cwsno = cws[0][index][:4]
     cwsM = findCwsMeta(cwsno)
-    concattedData05.append(["CWS", "27.06.2019 05:00", cws[10][index], cwsM[0], cwsM[1], cwsM[2]])
+    concattedData05.append(["CWS", cws[10][index-1], cws[10][index], cwsM[0], cwsM[1], cwsM[2]])
     index += 2
 
 with open("Data/concattedData05.csv", 'w', newline="") as output:
