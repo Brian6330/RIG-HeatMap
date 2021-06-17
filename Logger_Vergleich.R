d6bb6f71b7d465e6df263390dc6e1ae329262866
Brian6330 <brian@schweigler.org>
2021-06-17 10:56:17 +0200

    Update function to accept 2 or three parameters

--- Logger_Vergleich.R
+++ Logger_Vergleich.R
@@ -7,20 +7,44 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 loggers <- read.csv(file = "output_reworked/0_pre_processing_orig/log/log.csv")
 time <- loggers$log.date_time_gmt_plus_2
 
-# log_A should be the "urban" one, log_B the other one. Title is also the filename
-plot_temp_comparison <- function(time, log_A, log_B, title) {
-	outfile = paste("Temperaturverlauf/", title, ".png", sep="")
+# log_A should be the "urban" one. Title is also the filename.
+plot_temp_comparison <-
+	function(time, log_A, log_B, log_C=NULL, title) {
+		outfile = paste("Temperaturverlauf/", title, ".png", sep = "")
 
-	data <- data.frame(time,log_A,log_B)
-	ggplot(data, aes(x=time, y=log_A, group = 1)) +
-		geom_line(aes(y=log_A), color="black") +
-		geom_line(aes(y=log_B), color="blue") +
-		theme_minimal() +
-		coord_cartesian(ylim = c(15, 40)) +
-		labs(y = "Temperatur in °C", x = "Zeit", title = title, subtitle = "Temperaturverlauf 06.26. 20 Uhr - 27.06. 16 Uhr, 2019. ") +
-		ggsave(outfile, dpi = "print")
+		if(is.null(log_C)){
+			data <- data.frame(time, log_A, log_B)
+			ggplot(data, aes(x = time, y = log_A, group = 1)) +
+				geom_line(aes(y = log_A), color = "black") +
+				geom_line(aes(y = log_B), color = "blue") +
+				theme_minimal() +
+				coord_cartesian(ylim = c(15, 40)) +
+				labs(
+					y = "Temperatur in °C",
+					x = "Zeit",
+					title = title,
+					subtitle = "Temperaturverlauf 06.26. 20 Uhr - 27.06. 16 Uhr, 2019. "
+				) + ggsave(outfile, dpi = "print")
+		} else {
+			data <- data.frame(time, log_A, log_B, log_C)
+			ggplot(data, aes(x = time, y = log_A, group = 1)) +
+				geom_line(aes(y = log_A), color = "black") +
+				geom_line(aes(y = log_B), color = "blue") +
+				geom_line(aes(y = log_C), color = "green") +
+				theme_minimal() +
+				coord_cartesian(ylim = c(15, 40)) +
+				labs(
+					y = "Temperatur in °C",
+					x = "Zeit",
+					title = title,
+					subtitle = "Temperaturverlauf 06.26. 20 Uhr - 27.06. 16 Uhr, 2019. "
+				) + ggsave(outfile, dpi = "print")
+		}
 }
 
 # Westside vs. Land
-plot_temp_comparison(time, loggers$Log_61, loggers$Log_52, "Loggerstation Westside vs. Loggerstation Bümpliz Land")
+plot_temp_comparison(time, loggers$Log_61, loggers$Log_52, title="Loggerstation Westside vs. Loggerstation Bümpliz Land")
+
+# Inselspital vs. Bremgartenfriedhof vs. Bremgartenwald
+plot_temp_comparison(time, loggers$Log_26, loggers$Log_9, loggers$Log_32, "Loggerstation Bremgartenfriedhof vs. Bremgartenwald vs. Loggerstation Inselspital")
 
