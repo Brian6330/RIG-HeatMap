818d374b3b2849b3e7ec17e0ec2bd58ce82922f2
Brian6330 <games@schweigler.org>
2021-06-17 14:47:27 +0200

    Funktion um Logger nach belieben zu vergleichen mit automatischer Legenden-Generation

--- Logger_Vergleich.R
+++ Logger_Vergleich.R
@@ -1,5 +1,6 @@
 # install libraries
 library("ggplot2")
+library("tidyverse")
 
 # SET WORKING DIRECTORY
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -10,47 +11,60 @@ time <- loggers$Time.HH
 # log_A should be the "urban" one. Title is also the filename.
 plot_temp_comparison <-
 	function(time, log_A, log_B, log_C=NULL, station_A, station_B, station_C=NULL) {
-		
 
 		if(is.null(log_C)){
-		  outfile = paste("Temperaturverlauf/", station_A, station_B, ".png", sep = "-")
+		  outfile = paste("Temperaturverlauf/", station_A, station_B, ".png", sep = "_")
 			data <- data.frame(time, log_A, log_B)
 			ggplot(data, aes(x = time, y = log_A, group = 1)) +
-				geom_line(aes(y = log_A, color = "station_A")) +
-				geom_line(aes(y = log_B, color = "station_B")) +
+				geom_line(aes(y = log_A, color = "black")) +
+				geom_line(aes(y = log_B, color = "blue")) +
 				theme_minimal() +
-				coord_cartesian(ylim = c(15, 40)) +
+				coord_cartesian(ylim = c(16, 38)) +
 			  scale_x_continuous(label = c("20:00", "01:00", "06:00", "11:00", "16:00")) +
-			  scale_color_manual(name = "Loggerstation", values = c(station_A = "black", station_B = "blue")) +
+			  scale_colour_manual(name = "Loggerstation", values = c("black","blue"), labels = c(station_A, station_B)) + 
 			  labs(
 					y = "Temperatur in Grad Celsius",
 					x = "Zeit",
 					title = "Temperaturverlauf",
-					subtitle = "06.26. 20 Uhr - 27.06. 16 Uhr, 2019. "
+					subtitle = "20:00 06.26.19  - 16:00 27.06.19 "
 				) + ggsave(outfile, dpi = "print")
 		} else {
-		  outfile = paste("Temperaturverlauf/", station_A, station_B, station_C, ".png", sep = "-")
+		  outfile = paste("Temperaturverlauf/", station_A, station_B, station_C, ".png", sep = "_")
 			data <- data.frame(time, log_A, log_B, log_C)
 			ggplot(data, aes(x = time, y = log_A, group = 1)) +
-				geom_line(aes(y = log_A, color = "station_A")) +
-				geom_line(aes(y = log_B, color = "station_B")) +
-				geom_line(aes(y = log_C, color = "station_C")) +
+				geom_line(aes(y = log_A, color = "black")) +
+				geom_line(aes(y = log_B, color = "blue")) +
+				geom_line(aes(y = log_C, color = "green")) +
 				theme_minimal() +
-				coord_cartesian(ylim = c(15, 40)) +
+				coord_cartesian(ylim = c(16, 38)) +
 			  scale_x_continuous(label = c("20:00", "01:00", "06:00", "11:00", "16:00")) +
-			  scale_color_manual(name = "Loggerstation", values = c(station_A = "black", station_B = "blue", station_C = "green")) +
+			  scale_color_manual(name = "Loggerstation", values = c("black", "blue", "green"), labels = c(station_A, station_B, station_C)) +
 			  labs(
 					y = "Temperatur in Grad Celsius",
 					x = "Zeit",
 					title = "Temperaturverlauf",
-					subtitle = "06.26. 20 Uhr - 27.06. 16 Uhr, 2019. "
+					subtitle = "20:00 06.26.19  - 16:00 27.06.19 "
 				) + ggsave(outfile, dpi = "print")
 		}
-}
+	}
 
 # Westside vs. Land
-plot_temp_comparison(time, loggers$Log_61, loggers$Log_52, station_A="Westside", station_B="Bumpliz")
+plot_temp_comparison(time, loggers$Log_61, loggers$Log_52, station_A="Westside", station_B="Umland Bumpliz")
 
 # Inselspital vs. Bremgartenfriedhof vs. Bremgartenwald
-plot_temp_comparison(time, loggers$Log_26, loggers$Log_9, loggers$Log_32, station_A="Bremgartenfriedhof", station_B="Bremgartenwald", station_C="Inselspital")
+plot_temp_comparison(time, loggers$Log_26, loggers$Log_9, loggers$Log_32, station_A="Inselspital", station_B="Bremgartenfriedhof", station_C="Bremgartenwald")
 
+# Schlossmatte Familiengärten vs. Europaplatz
+plot_temp_comparison(time, loggers$Log_59, loggers$Log_2, station_A="Schlossmatte Familiengarten", station_B="Europaplatz")
+
+# Viktoriarein vs. Kasernenareal
+plot_temp_comparison(time, loggers$Log_36, loggers$Log_34, station_A="Viktoriarein", station_B="Kasernenareal")
+
+# Galgenfeld Neubausidelung vs. Galgenfeldindustrie vs. Rosengarten
+plot_temp_comparison(time, loggers$Log_19, loggers$Log_30, loggers$Log_17, station_A="Galgenfeld Neubausiedlung", station_B="Galgenfeldindustrie", station_C="Rosengarten")
+
+# Helvetiaplatz vs. Egelsee-Siedlung  vs.  Egelsee
+plot_temp_comparison(time, loggers$Log_14, loggers$Log_12, loggers$Log_13, station_A="Helvetiaplatz", station_B="Egelsee Siedlung", station_C="Egelsee")
+
+# Thunplatz vs. Dalholzli-Wald
+plot_temp_comparison(time, loggers$Log_82, loggers$Log_33, station_A="Thunplatz", station_B="Dalholzli-Wald")
